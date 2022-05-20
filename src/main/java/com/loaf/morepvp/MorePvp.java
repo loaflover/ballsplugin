89e94edf2a3da20d7cf3cc17e965414a3a022d0a
loaflover <34983803+loaflover@users.noreply.github.com>
2022-05-20 19:28:03 +0300

    set the attrebuta adder

--- src/main/java/com/loaf/morepvp/MorePvp.java
+++ src/main/java/com/loaf/morepvp/MorePvp.java
@@ -1,17 +1,38 @@
 package com.loaf.morepvp;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import java.util.UUID;
 
+import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.attribute.AttributeModifier.Operation;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
 public final class MorePvp extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    
+    public void PlayerToggleSneakEvent(Player player, boolean isSneaking) {
+        if (player.getInventory().getItemInMainHand().getType().equals(Material.AIR)){
+            player.sendMessage("hold a damn item");
+            return;
+        }
+        AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(), "generic.health", 100.0, Operation.ADD_NUMBER, EquipmentSlot.HAND);
+        ItemStack item = player.getInventory().getItemInMainHand();
+        ItemMeta meta = item.getItemMeta();
+
+        meta.addAttributeModifier(Attribute.GENERIC_MAX_HEALTH, modifier);
     }
 }
