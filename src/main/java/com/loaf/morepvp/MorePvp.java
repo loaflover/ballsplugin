01a6b70edca6fc47f5ba32f31c9367e0d6d297a9
loaflover <34983803+loaflover@users.noreply.github.com>
2022-05-20 18:47:20 +0300

    redid entire thing
    hated how messy the old code was so just replaced it with new plugin. old code was kept in a folder if we need it

---
+++ src/main/java/com/loaf/morepvp/MorePvp.java
@@ -0,0 +1,17 @@
+package com.loaf.morepvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MorePvp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
